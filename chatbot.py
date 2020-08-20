f374ded1b85632a031cacae9fe9169845dce5562
“zhanghao” <zh@nosugartech.com>
2020-08-20 15:04:32 +0800

    feat: add chatbot main entry

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+from MachineLearning.analyze_question import AnalysisQuestion
+from KnowledgeGraph.get_answer import Get_answer
+
+if __name__ == "__main__":
+    aq = AnalysisQuestion()
+    ga = Get_answer()
+    while True:
+        question = input('请输入你想查询的信息：')  # 成龙 和 李连杰合作过的电影有哪些？
+        index, params = aq.analysis(question)
+        answers = ga.get_data(index, params)
+        print('答案:')
+        for ans in answers:
+            print(ans[0])
