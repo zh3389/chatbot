8175fb8bded02668d514574f40306cff52b1d1aa
zh <zhanghao_3389@163.com>
2020-12-05 13:16:45 +0800

    feat: 更新README.md

--- KnowledgeGraph/get_answer.py
+++ KnowledgeGraph/get_answer.py
@@ -8,41 +8,41 @@ class Get_answer():
     def get_data(self, index, params):
         query = ''
         if index == 0:
-            query = "match(m:Movie) where m.title='{}' return  m.rating;".format(params[0])  # 评分
+            query = "MATCH (m:Movie) WHERE m.title='{}' RETURN  m.rating;".format(params[0])  # 评分
         elif index == 1:
-            query = "match(m:Movie) where m.title='{}' return  m.releasedate;".format(params[0])  # 上映时间
+            query = "MATCH (m:Movie) WHERE m.title='{}' RETURN  m.releasedate;".format(params[0])  # 上映时间
         elif index == 2:
-            query = "match(m:Movie)-[r:is]->(g:Genre) where m.title='{}' return g.name;".format(params[0])  # 类型
+            query = "MATCH (m:Movie)-[r:is]->(g:Genre) WHERE m.title='{}' RETURN g.name;".format(params[0])  # 类型
         elif index == 3:
-            query = "match(m:Movie) where m.title='{}' return  m.introduction;".format(params[0])  # 简介
+            query = "MATCH (m:Movie) WHERE m.title='{}' RETURN  m.introduction;".format(params[0])  # 简介
         elif index == 4:
-            query = "MATCH (p:Person)-[r:actedin]->(m:Movie) where m.title='{}' RETURN p.name;".format(
+            query = "MATCH (p:Person)-[r:actedin]->(m:Movie) WHERE m.title='{}' RETURN p.name;".format(
                 params[0])  # 演员列表
         elif index == 5:
-            query = "match(p:Person) where p.name='{}'  return p.biography;".format(params[0])  # 演员介绍
+            query = "MATCH (p:Person) WHERE p.name='{}'  RETURN p.biography;".format(params[0])  # 演员介绍
         elif index == 6:
-            query = "match(n:Person)-[:actedin]-(m:Movie) where n.name ='{}' " \
-                    "match(g:Genre)-[:is]-(m) where g.name='{}' return distinct  m.title".format(params[0], params[
+            query = "MATCH(n:Person)-[:actedin]-(m:Movie) WHERE n.name ='{}' " \
+                    "MATCH(g:Genre)-[:is]-(m) WHERE g.name='{}' RETURN distinct  m.title".format(params[0], params[
                 1])  # 某演员演过什么类型电影有哪些
         elif index == 7:
-            query = "match(n:Person)-[:actedin]->(m:Movie) where n.name='{}' return m.title".format(
+            query = "MATCH(n:Person)-[:actedin]->(m:Movie) WHERE n.name='{}' RETURN m.title".format(
                 params[0])  # 某演员演过什么电影
         elif index == 8:
-            query = "match(n:Person)-[:actedin]-(m:Movie) where n.name ='{}' and m.rating> {} return m.title".format(
+            query = "MATCH(n:Person)-[:actedin]-(m:Movie) WHERE n.name ='{}' and m.rating> {} RETURN m.title".format(
                 params[0], params[1])  # 参演评分 大于 x
         elif index == 9:
-            query = "match(n:Person)-[:actedin]-(m:Movie) where n.name ='{}' and m.rating < {} return m.title;".format(
+            query = "MATCH(n:Person)-[:actedin]-(m:Movie) WHERE n.name ='{}' and m.rating < {} RETURN m.title;".format(
                 params[0], params[1])  # 参演评分 小于 x
         elif index == 10:
-            query = "match(n:Person)-[:actedin]-(m:Movie) where n.name ='{}' " \
-                    "match(p:Genre)-[:is]-(m) return distinct  p.name".format(params[0])  # 演员演过哪些类型电影
+            query = "MATCH(n:Person)-[:actedin]-(m:Movie) WHERE n.name ='{}' " \
+                    "MATCH(p:Genre)-[:is]-(m) RETURN distinct  p.name".format(params[0])  # 演员演过哪些类型电影
         elif index == 11:
-            query = "match(p1:Person)-[:actedin]->(m:Movie) match(p2:Person)-[:actedin]->(m)" \
-                    "where p1.name='{}' and p2.name='{}' return m.title;".format(params[0], params[1])  # 演员a和演员b合作过的电影
+            query = "MATCH(p1:Person)-[:actedin]->(m:Movie) MATCH(p2:Person)-[:actedin]->(m)" \
+                    "WHERE p1.name='{}' and p2.name='{}' RETURN m.title;".format(params[0], params[1])  # 演员a和演员b合作过的电影
         elif index == 12:
-            query = "match(n)-[:actedin]-(m) where n.name ='{}' return count(*);".format(params[0])  # 演员演过电影数量
+            query = "MATCH(n)-[:actedin]-(m) WHERE n.name ='{}' RETURN count(*);".format(params[0])  # 演员演过电影数量
         elif index == 13:
-            query = "match(n:Person) where n.name='{}' return n.birth;".format(params[0])  # 演员出生日期
+            query = "MATCH(n:Person) WHERE n.name='{}' RETURN n.birth;".format(params[0])  # 演员出生日期
 
         result = self.graph.run(query)
         return result
--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 **如果本项目对你学习构建一个聊天机器人有帮助, 欢迎 Start...**
 
+#### 如何构建
+
+1. 先构建知识库参考[知识图谱](https://github.com/Mrzhang3389/chatbot/tree/master/KnowledgeGraph)
+
+2. 再构建问题处理模块参考[机器学习](https://github.com/Mrzhang3389/chatbot/tree/master/MachineLearning)
+
+3. 然后 问题处理模块 + 答案搜索模块 = 基于知识库的问答机器人
+
+4. 最后优化模型中的细节和捕获异常
+
 #### 环境搭建
 
 neo4j环境搭建参考: [知识图谱篇](https://github.com/Mrzhang3389/chatbot/tree/master/KnowledgeGraph)
@@ -20,14 +30,3 @@ python chatbot.py
 
 ![example](assets/example.png)
 
-![example2](assets/example2.png)
-
-![example3](assets/example3.png)
-
-#### 如何构建
-
-1. 先构建知识库参考[知识图谱](https://github.com/Mrzhang3389/chatbot/tree/master/KnowledgeGraph)
-
-2. 再构建问题处理模块参考[机器学习](https://github.com/Mrzhang3389/chatbot/tree/master/MachineLearning)
-3. 然后 问题处理模块 + 答案搜索模块 = 基于知识库的问答机器人
-4. 最后优化模型中的细节和捕获异常
